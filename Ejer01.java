e94ec130925135a76090a4578a7d1916826c90f6
SebastianAsisRomero <49380593+SebastianAsisRomero@users.noreply.github.com>
2019-05-27 11:37:08 -0500

    Add files via upload

---
+++ Ejer01.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjercicioLibroCapitulo1;
+
+/**
+ *
+ * @author Diego
+ */
+public class Ejer01 {
+        public static void main(String[] args){
+            System.out.println("Welcome to Java ");
+            System.out.println("Welcome to Computer Science ");
+            System.out.println("Programming is fun ");
+        }    
+}
